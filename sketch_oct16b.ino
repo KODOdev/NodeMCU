232264af8818bdb680dbde84cd20cea606ed4494
Daniel Gads <danielgads@hotmail.com>
2018-10-23 15:53:41 -0300

    Add files via upload

--- sketch_oct16b.ino
+++ sketch_oct16b.ino
@@ -1,25 +1,26 @@
 #include <ESP8266WiFi.h>
- 
-  
+
+//declaracao da rede
  
 const char* ssid = "AndroidAP"; // Nome da Rede
  
 const char* password = "tampa123"; //Password da rede
  
-WiFiServer server(80); // Porto 80
- 
+WiFiServer server(80); // Porta de conexao
   
- 
 void setup() {
+
+//infos do log
  
-Serial.begin(115200);
+Serial.begin(115200); //velocidade do log
  
-delay(10);
- 
-pinMode(LED_BUILTIN, OUTPUT); // Define o D7 como saída
+delay(10); //atraso entre informacoes
+
+//informacoes iniciais do led
+
+pinMode(LED_BUILTIN, OUTPUT); // Define o led ja embutido na placa como saída
  
 digitalWrite(LED_BUILTIN, HIGH); // O LED começa desligado
-  
  
 // Comunicação com a rede WiFi
  
@@ -30,9 +31,7 @@ Serial.println();
 Serial.print("Connecting to "); // Mensagem apresentada no monitor série
  
 Serial.println(ssid); // Apresenta o nome da rede no monitor série
- 
   
- 
 WiFi.begin(ssid, password); // Inicia a ligação a rede
  
 while (WiFi.status() != WL_CONNECTED) {
@@ -47,15 +46,11 @@ Serial.println("");
  
 Serial.println("WiFi connected"); // Se a ligação é efectuada com sucesso apresenta esta mensagem no monitor série
  
-  
- 
 // Servidor
  
 server.begin(); // Comunicação com o servidor
  
 Serial.println("Servidor iniciado"); //é apresentado no monitor série que o  servidor foi iniciado
- 
-  
  
 // Impressão do endereço IP
  
@@ -66,12 +61,7 @@ Serial.print("http://");
 Serial.print(WiFi.localIP()); //Abrindo o Brower com este IP acedemos á pagina HTML de controlo dos LED´s, sendo que este IP só está disponível na rede à qual o ESP8266 se encontra ligado
  
 Serial.println("/");
- 
-  
- 
 }
- 
-  
  
 void loop() {
  
@@ -111,8 +101,6 @@ client.flush();
  
 // Operação do pedido
  
-  
- 
 int value = LOW;
  
 if (request.indexOf("/LED=ON") != -1)  {
@@ -131,11 +119,8 @@ value = LOW;
  
 }
  
-  
  
 // Inicialização da página HTML
- 
-  
  
 // Retorno do resposta
  
@@ -149,11 +134,7 @@ client.println("<!DOCTYPE HTML>");
  
 client.println("<html>");
  
-  
- 
 client.print("Estado do LED: ");
- 
-  
  
 if(value == HIGH) { // Se está ligado apresenta “on”
  
@@ -167,19 +148,14 @@ client.print("Off");// Se está desligado apresenta “Off”
  
 client.println("<br><br>");
 
-client.println("<a href=\"/LED=ON\"\"><button>Turn Off</button></a><br/>");  // Desligar o LED corresponde Turn Off
-client.println("<a href=\"/LED=OFF\"\"><button>Turn On</button></a>");// Ligar o LED corresponde Turn On
+client.println("<a href=\"/LED=ON\"\"><button>Turn On</button></a><br/>");  // Ligar o LED corresponde Turn On
+client.println("<a href=\"/LED=OFF\"\"><button>Turn Off</button></a>"); // Desligar o LED corresponde Turn Off
  
 client.println("</html>");
- 
-  
  
 delay(1);
  
 Serial.println("Cliente desconectado"); // Depois do cliente efectuar o pedido apresenta esta mensagem no monitor série
  
 Serial.println("");
- 
-  
- 
 }
